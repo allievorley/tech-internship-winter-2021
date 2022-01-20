120fdd86c748a49951ad0fdf16d8242ecf9f0a69
NetaShubin <shubinneta@gmail.com>
2022-01-20 10:18:31 -0800

    Created home page layout

--- js/neta.js
+++ js/neta.js
@@ -3,12 +3,13 @@ function EditActivities() {
     let myIcon = document.getElementById("icon");
     let myList = document.getElementById("activitiesList");
     if (myList.contentEditable == "false") {
-        icon.src = "../images/doneIcon.png";
+        icon.src = "images/doneIcon.png";
         icon.alt = "Save";
         myList.contentEditable = "true";
     } else {
-        icon.src = "../images/editIcon.png";
+        icon.src = "images/editIcon.png";
         icon.alt = "Edit";
         myList.contentEditable = "false";
+        alert("Edits saved!");
     }
 }
