017278376bf649164f73ef0270d4a966dadd238e
dgoetz987 <87041840+dgoetz987@users.noreply.github.com>
2022-01-23 22:57:23 -0800

    Structural Stuff
    Barebones structure of how I want to write this code, will talk to relevant people later

--- js/drew.js
+++ js/drew.js
@@ -1,4 +1,17 @@
-// Function to get timer working and doing an alert when the timer ends
+let time = 0;
+
+// Function called every second that gets Eric's timer. Will also have list of times to trigger to compare
+// We probably need a stack or something with every 'event'
 function TimerUpdate() {
+    document.write("working");
+}
+
+// Function to display the activity
+function Popup(activity) {
+
+}
+
+// Function to play the sound
+function AlertSound() {
 
 }
