d0867214f2d480405a77596d8df1f1316fe2cd77
dgoetz987 <87041840+dgoetz987@users.noreply.github.com>
2022-01-24 16:42:40 -0800

    OOP Overhaul
    I have added an OOP overhaul / prototype into my codebase. Right now, there is no collection of the event metadata anywhere in the program. Essentially meaning I have no times or events to relate to each other.
    
    I have added a framework that allows for basic event objects to have a start time, end time, name, and description. Do we want to collect this data? If so how? All I have created is a framework to store it. We do this, or I will just post incremental reminders every x minutes (make this time adjustable) that has a generic message.

--- js/drew.js
+++ js/drew.js
@@ -1,13 +1,59 @@
-let time = 0;
+// main clock that checks events
+let [mainHours, mainMinutes, mainSeconds] = [0, 0, 0];
+// array of events and their data
+let timerEvents = []
 
-// Function called every second that gets Eric's timer. Will also have list of times to trigger to compare
-// We probably need a stack or something with every 'event'
+// ___________ Optional OOP stuff I would like to have implemented throughout ___________
+
+// event class to hold events and their relevant metadata
+class timerEvent {
+    constructor(name, description, startTime, endTime) {
+        this.name = name;
+        this.description = description;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+}
+
+// class to carry times easily for timerEvent class
+class time {
+    constructor(hours, minutes, seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = this.seconds;
+    }
+}
+
+// create a new event with given parameters
+function createNewEvent(name, description, startTime, endTime) {
+    timerEvents.push(new timerEvent(name, description, startTime, endTime));
+}
+
+// _______________ Normal Stuff resumes below _____________________
+
+// main function that orients the program
+function main() {
+    TimerUpdate();
+    CheckIfEventShouldFire(mainHours, mainMinutes, mainSeconds, timerEvents);
+}
+
+// update the main body timer
 function TimerUpdate() {
-    document.write("working");
+    dt = new Date($.now());
+    mainHours = dt.getHours();
+    mainMinutes = dt.getMinutes();
+    mainSeconds = dt.getSeconds();
+}
+
+// compare event timings to the time
+function CheckIfEventShouldFire(hours, minutes, seconds, events) {
+    events.forEach(element => {
+        if (element.hours);
+    });
 }
 
 // Function to display the activity
-function Popup(activity) {
+function Popup() {
 
 }
 
